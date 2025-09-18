0f10b3970479ab89ba0d844c7f5741c500eee50b
haj <vne4812@gmail.com>
2025-09-18 15:25:13 +0000

    make csv visualization in gui

--- data_visualizator/gui/main_window.py
+++ data_visualizator/gui/main_window.py
@@ -1,16 +1,26 @@
 import sys
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QApplication
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QApplication, QSplitter
+from PySide6 import QtCore
 
 from .explorer import Explorer
+from .tables import DataSetViewer
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
         self.explorer = Explorer(self)
+        self.dataset_viewer = DataSetViewer(self)
+
+        self.splitter = QSplitter(QtCore.Qt.Orientation.Horizontal)
+        self.splitter.addWidget(self.explorer)
+        self.splitter.addWidget(self.dataset_viewer)
 
         self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.explorer)
+        self.layout.addWidget(self.splitter)
+    
+    def open_dataset(self, path):
+        self.dataset_viewer.open_dataset(path)
 
 
 def start():
