2bf9e4c2e02aae183a42c822699104a24ea9d016
haj <vne4812@gmail.com>
2025-09-20 14:51:05 +0000

    I removed the пгш in order to write it again, but correctly.

--- data_visualizator/gui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Пакет GUI для приложения визуализации данных.
-
-Этот модуль делает доступной функцию `start` из `main_window` на уровне пакета,
-чтобы упростить запуск приложения.
-"""
-
-from .main_window import start
-from ._base import BaseWidget
--- data_visualizator/gui/menu_bar/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Пакет для элементов строки меню.
-
-Этот модуль делает классы `FileMenu` и `ViewMenu` доступными
-на уровне пакета `menu_bar` для удобства импорта.
-"""
-
-from .file_menu import FileMenu
-from .view_menu import ViewMenu
