713b39e22f41a8cbd811a239e151b0a9f18bb6ac
Gadzhimet Mursalov <vne4812@gmail.com>
2025-09-27 15:17:14 +0300

    update config enter point

--- data_visualizator/config/__init__.py
+++ data_visualizator/config/__init__.py
@@ -1,2 +1,3 @@
-from .base_config import SupportedModels, AllParamNames
+from .base_config import AllParamNames
+from .available_models import SupportedModels
 from .models_supported_params import ALL_PARAMS, ModelsSupportedParams
