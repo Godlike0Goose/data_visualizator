bc7449c47916bd75657095c4bdefeb12549e4f57
GodlikeGoose <aliislamdzafarov054@gmail.com>
2025-09-27 15:25:23 +0300

    new utils

--- data_visualizator/utils.py
+++ data_visualizator/utils.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 import pandas as pd
+from .config import AllParamNames, SupportedModels, ModelsSupportedParams
+
+def get_params_for_model(model: SupportedModels) -> set:
+    """Возвращает множество поддерживаемых параметров для указанной модели."""
+
+    if not isinstance(model, SupportedModels):
+        raise TypeError("Model must be of type SupportedModels")
+
+    model_name = model.name
+    return ModelsSupportedParams[model_name].value
 
 def read_data_file(file_path: str):
     '''проверяет тип файла и открывает его
