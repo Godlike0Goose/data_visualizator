ddb1d2ae71eec17ed77bc2a4771a6049e3defa1b
GodlikeGoose <aliislamdzafarov054@gmail.com>
2025-09-27 15:46:19 +0300

    static Datafilter

--- data_visualizator/datasetFilter.py
+++ data_visualizator/datasetFilter.py
@@ -2,24 +2,28 @@ import pandas as pd
 
 
 class DatasetFilter:
-    def __init__(self, df):
-        self._df = df
+    @staticmethod
+    def remove_empty_data(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Удаляет строки, содержащие хотя бы одно пустое (NaN) значение.
+        и возвращает  без строк с пропущенными значениями.
+        """
+        return df.dropna()
 
-    def Delite_empty_data(self):
-        """передается dataframe который преобразуется в dataframe
-        без строк с пустыми значениями"""
-        df_clean = self.df.dropna()
-        return df_clean
-
-    def One_hot_encoding(self, df):
+    @staticmethod
+    def one_hot_encoding(df: pd.DataFrame) -> pd.DataFrame:
         """использует метод OHE для преобразования
          котегориальных переменныв некотегорельные
         путем преобразования тех самы котегориальных
             значений в колонки с данными 1 и 0"""
         categorical_cols = df.select_dtypes(exclude=["number", "bool"]).columns.tolist()
+        
         if not categorical_cols:
             return df.copy()
-        df_encoded = pd.get_dummies(
-            df, columns=categorical_cols, drop_first=False, dtype=int
+        
+        return pd.get_dummies(
+            df,
+            columns=categorical_cols,
+            drop_first=False,
+            dtype=int
         )
-        return df_encoded
