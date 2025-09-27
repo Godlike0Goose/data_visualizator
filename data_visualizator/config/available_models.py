02bfd38e4a1fea712ba0a62f6e20f9ffad7d52a0
Gadzhimet Mursalov <vne4812@gmail.com>
2025-09-27 15:17:55 +0300

    add available  models enum

---
+++ data_visualizator/config/available_models.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+from sklearn.linear_model import (
+    Ridge, 
+    Lasso, 
+    ElasticNet
+)
+
+class SupportedModels(Enum):
+    Ridge_model = Ridge
+    Lasso_model = Lasso
+    ElasticNet_model = ElasticNet
+
+
