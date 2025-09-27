8576051b6fddcd633b1c58d64b133490b8943491
Gadzhimet Mursalov <vne4812@gmail.com>
2025-09-27 09:59:34 +0300

    update base config

--- data_visualizator/config/base_config.py
+++ data_visualizator/config/base_config.py
@@ -1,7 +1,4 @@
-from enum import Enum
-
-
-class SupportedModels(Enum):
-    Ridge_model = 0
-    Lasso_model = 1
-    Elastic_Net_model = 2
+class SupportedModels:
+    Ridge_model = "Ridge"
+    Lasso_model = "Lasso"
+    Elastic_Net_model = "ElasticNet"
